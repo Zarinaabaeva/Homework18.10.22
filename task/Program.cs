b6a1c2d417a54d89456a5aacf3011b3ab9db675e
Zarina <abaevazarina@yandex.ru>
2022-10-17 15:05:50 +0300

    task solved

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] text(int length)
+{
+    string[] arr = new string[] { "hello", "2", "the", "world", ":-)" };
+
+    return arr;
+}
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] NewText(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+            Console.Write($"{arr[i]} ");
+    }
+
+    return arr;
+}
+
+string[] arr1 = text(5);
+PrintArray(arr1);
+string[] result = NewText(arr1);
